79c181101a502947880be20218125639aad17bc5
bamacaerick <bamacaerick@gmail.com>
2024-04-21 23:30:18 -0600

    Add new footer structure

--- footer.php
+++ footer.php
@@ -1,10 +1,12 @@
-<?php //get_sidebar(); ?>
-<footer id="footer" role="contentinfo" class="container">
-    <div id="copyright">
-        &copy; <?php echo esc_html( date_i18n( __( 'Y', 'generic' ) ) ); ?>
-        <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
-    </div>
-</footer>
+    <?php //get_sidebar(); ?>
+    <footer id="footer" role="contentinfo" class="container">
+        <div id="copyright">
+            &copy; <?php echo esc_html( date_i18n( __( 'Y', 'generic' ) ) ); ?>
+            <?php echo esc_html( get_bloginfo( 'name' ) ); ?>
+        </div>
+    </footer>
+</div> 
+<!-- Closing container -->
 <?php wp_footer(); ?>
 </body>
 

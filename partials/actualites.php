667049af3c1e328653054bc3e9aa1a3fcaa4eee4
bamacaerick <bamacaerick@gmail.com>
2024-04-27 10:26:56 -0600

    Update actualites partial structure

--- partials/actualites.php
+++ partials/actualites.php
@@ -1,6 +1,6 @@
 <?php
 // query recent blog post
-$getBlogs = new WP_Query(array(
+$getActualites = new WP_Query(array(
     'category_name' => 'actualites',
     'posts_per_page' => 2,
     'orderby' => 'date',
@@ -8,17 +8,17 @@ $getBlogs = new WP_Query(array(
 ));
 ?>
 
-<?php if ($getBlogs->have_posts()): ?>
-    <div class="article article-blogs">
-        <div class="article-header">
-            <h2 class="text-uppercase text-center pt-sans-narrow-bold text-gray-200 h1"><span class="hexagon">Actualités</span></h2>
+<?php if ($getActualites->have_posts()): ?>
+    <div class="article-actualites">
+        <div class="article-actualites-header">
+            <h2 class="article-actualites-header-heading text-uppercase text-center pt-sans-narrow-bold text-gray-200 h1"><span class="hexagon">Actualités</span></h2>
         </div>
-        <?php while ($getBlogs->have_posts()): $getBlogs->the_post(); ?>
+        <?php while ($getActualites->have_posts()): $getActualites->the_post(); ?>
             <?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
-            <div class="article-body">
+            <div class="article-actualites-body">
                 <div class="row">
                     <div class="col-12 col-md-auto">
-                        <div class="article-thumbnail">
+                        <div class="article-actualites-thumbnail">
                             <?php if($featured_img_url) :?>   
                               <img src="<?php echo $featured_img_url; ?>" alt="">
                             <?php else: ?>
@@ -27,20 +27,20 @@ $getBlogs = new WP_Query(array(
                         </div>
                     </div>
                     <div class="col-12 col-md">
-                        <div class="article-content">
-                            <span class="d-block"><?php the_date(); ?></span>
-                            <h3><?php the_title(); ?></h3>
-                            <?php the_excerpt(); ?>
-                            <div class="article-permalink">
-                                <a href="<?php the_permalink(); ?>"><span class="sr-only">Read more</span>...</a>
+                        <div class="article-actualites-content">
+                            <span class="d-block text-black fw-bold"><?php echo get_the_date(); ?></span>
+                            <h3 class="text-green text-uppercase"><?php the_title(); ?></h3>
+                            <p class="text-gray-body m-0"><?php echo get_the_excerpt(); ?></p>
+                            <div class="article-actualites-permalink text-end">
+                                <a href="<?php the_permalink(); ?>" class="article-actualites-permalink-anchor text-green fs-50"><span class="sr-only">Read more</span>...</a>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         <?php endwhile; ?>
-        <div class="article-archive">
-            <a href="#">Lire toutes les actualités</a>
+        <div class="article-actualites-archive text-end">
+            <a href="#" class="article-actualites-archive-anchor">> Lire toutes les actualités</a>
         </div>
     </div>
 <?php endif; ?>

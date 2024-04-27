8c63850d0a2ac24f275aa0f5ff3796468b15af60
bamacaerick <bamacaerick@gmail.com>
2024-04-27 10:38:52 -0600

    Add blog partial query

--- partials/blogs.php
+++ partials/blogs.php
@@ -9,32 +9,32 @@
 ?>
 
 <?php if ($getBlogs->have_posts()): ?>
-    <div class="article article-blogs">
-        <div class="article-header">
-            <h2>Blogs des classes</h2>
+    <div class="article-blogs">
+        <div class="article-blogs-header bg-pink">
+            <h2 class="article-blogs-header-heading pt-sans-narrow-bold text-uppercase h3 m-0">Blogs des classes</h2>
         </div>
         <?php while ($getBlogs->have_posts()): $getBlogs->the_post(); ?>
             <?php $featured_img_url = get_the_post_thumbnail_url(get_the_ID(), 'full'); ?>
-            <div class="article-body">
-                <div class="article-thumbnail">
+            <div class="article-blogs-body">
+                <div class="article-blogs-thumbnail">
                     <?php if($featured_img_url) :?>   
                       <img src="<?php echo $featured_img_url; ?>" alt="">
                     <?php else: ?>
                       <img  src="<?php bloginfo('stylesheet_directory'); ?>/images/image-placeholder-2.jpg" alt="" />
                     <?php endif; ?>
                 </div>
-                <div class="article-content">
-                    <span class="d-block"><?php the_date(); ?></span>
-                    <h3><?php the_title(); ?></h3>
-                    <?php the_excerpt(); ?>
-                    <div class="article-permalink">
-                        <a href="<?php the_permalink(); ?>"><span class="sr-only">Read more</span>...</a>
+                <div class="article-blogs-content">
+                    <span class="article-blogs-content-date d-block"><?php echo get_the_date(); ?></span>
+                    <h3 class="article-blogs-content-heading text-uppercase"><?php the_title(); ?></h3>
+                    <p class="article-blogs-content-excerpt"><?php echo wp_trim_words(get_the_excerpt(), 34); ?></p>
+                    <div class="article-blogs-permalink text-end">
+                        <a href="<?php the_permalink(); ?>" class="article-blogs-permalink-anchor text-pink"><span class="sr-only">Read more</span>...</a>
                     </div>
                 </div>
             </div>
         <?php endwhile; ?>
-        <div class="article-archive">
-            <a href="#">Voir tous les blogs</a>
+        <div class="article-blogs-archive text-end">
+            <a href="#" class="article-blogs-archive-anchor">> Voir tous les blogs</a>
         </div>
     </div>
 <?php endif; ?>

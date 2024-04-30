eb7a7589d5da645c84621f0767874f4e0a515e12
bamacaerick <bamacaerick@gmail.com>
2024-04-29 22:08:41 -0600

    Initial structure to show stats widget

---
+++ partials/stats.php
@@ -0,0 +1,9 @@
+<div class="row">
+    126   personnels
+1000 eleves
+81%  de nos élèves poursuivent leurs études à l’étranger
+    <div class="col-12 col-md-3"></div>
+    <div class="col-12 col-md-3"></div>
+    <div class="col-12 col-md-3"></div>
+    <div class="col-12 col-md-3"></div>
+</div>

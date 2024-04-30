45c738eb9dcca40b18aa02b9f4091940c31b93bd
bamacaerick <bamacaerick@gmail.com>
2024-04-29 23:35:21 -0600

    Add actualites styles

---
+++ entry-actualites.php
@@ -0,0 +1,15 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class('entry-actualites'); ?>>
+    <div class="entry-content container" itemprop="mainEntityOfPage">
+        <header class="entry-actualites-header">
+            <h1 class="entry-actualites-heading text-uppercase text-center pt-sans-narrow-bold text-green">
+                <span class="hexagon"><?php the_title(); ?></span>
+            </h1>
+            <?php $thePostDate = get_the_date('d F Y'); ?>
+            <time class="entry-actualites-date" datetime="<?php echo $thePostDate; ?>"><?php echo $thePostDate; ?></time>
+        </header>
+        <meta itemprop="description" content="<?php echo wp_strip_all_tags( get_the_excerpt(), true ); ?>" />
+        <div class="entry-actualites-content wp_editor">
+            <?php the_content(); ?>
+        </div>
+    </div>
+</article>

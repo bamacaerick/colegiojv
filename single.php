459fc82cac5836a9078c58c9b68ae7958e6817ca
bamacaerick <bamacaerick@gmail.com>
2024-05-01 00:25:16 -0600

    Add sidebar to single and page home

--- single.php
+++ single.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
-<main id="content" role="main" class="singular-content">
+<main id="content" role="main" class="singular-content row">
     <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+    <div class="col-12 col-md-8">
         <?php get_template_part('entry-actualites'); ?>
+    </div>
     <?php endwhile; endif; ?>
+    <div class="col-12 col-md-4">
+        <?php get_sidebar('links'); ?>
+    </div>
 </main>
 <?php get_footer(); ?>
